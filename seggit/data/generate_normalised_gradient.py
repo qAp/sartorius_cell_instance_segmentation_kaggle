a6841cf4e7e9fd452666a7dccfb8297c6618b4f7
Jack Yu <jackchungchiehyu@gmail.com>
2021-11-18 05:59:26 +0800

    Script to generate uvec
    uvec = normalised gradient of distance transform

---
+++ seggit/data/generate_normalised_gradient.py
@@ -0,0 +1,22 @@
+
+import os
+import multiprocessing
+from tqdm.auto import tqdm
+from seggit.data.config import DIR_DTFM
+from seggit.data.util import _generate_normalised_gradient
+
+dir_uvec = '/kaggle/working/uvec'
+
+imgids = [fn.split('.')[0] for fn in os.listdir(DIR_DTFM)]
+args_list = [(imgid, dir_uvec) for imgid in imgids]
+
+os.makedirs(dir_uvec, exist_ok=True)
+
+p = multiprocessing.Pool(processes=os.cpu_count())
+
+with tqdm(total=len(args_list)) as pbar:
+    for imgid in p.imap(_generate_normalised_gradient, args_list):
+        pbar.set_description(f'Processing {imgid}')
+        pbar.udpate(1)
+
+p.close()
