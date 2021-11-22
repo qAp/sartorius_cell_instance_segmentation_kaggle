8d5b2ad2927aa97c8c39c0adbf2eab4eeb40c3c2
Jack Yu <jackchungchiehyu@gmail.com>
2021-11-22 12:49:35 +0800

    DBG. Add parser arg `one_cycle_total_steps`

--- seggit/lit_models/instance_direction_litmodel.py
+++ seggit/lit_models/instance_direction_litmodel.py
@@ -34,7 +34,9 @@ class InstanceDirectionLitModel(pl.LightningModule):
     @staticmethod
     def add_argparse_args(parser):
         parser.add_argument('--lr', type=float, default=LR)
-        parser.add_argmuent('--optimizer', type=str, default='optimizer')
+        parser.add_argmuent('--optimizer', type=str, default=OPTIMIZER)
+        parser.add_argument('--one_cycle_total_steps', 
+                            type=int, default=ONE_CYCLE_TOTAL_STEPS)
 
     def configure_optimizers(self):
         optimizer = self.optimizer(self.parameters(), 
