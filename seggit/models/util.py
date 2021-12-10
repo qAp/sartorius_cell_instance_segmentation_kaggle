d3836c300551af6086cd270b8a1c8e29420a2753
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-10 21:44:00 +0800

    Create smp model from data.config and args

---
+++ seggit/models/util.py
@@ -0,0 +1,25 @@
+
+import segmentation_models_pytorch as smp
+
+
+def create_segmentation_model(data_config, args=None):
+
+    arch = args.arch
+    encoder_name = args.encoder_name
+    in_channels = data_config['input_dims'][2]
+    classes = data_config['output_dims'][2]
+
+    if classes == 3:
+        activation = 'softmax'
+    else:
+        activation = 'sigmoid'
+
+    model = arch(encoder_name=encoder_name,
+                 in_channels=in_channels,
+                 classes=classes,
+                 activation=activation)
+
+    return model
+
+
+    
