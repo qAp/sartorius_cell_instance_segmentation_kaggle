e2987eed229d124209319843f51930b114161387
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-13 21:40:47 +0800

    Script to generate 3-channel semseg masks
    Channel 0: cell mass
    Channel 1: cell wall overlapping cell mass
    Channel 2: everything else

--- seggit/data/scripts/make_semseg_target.py
+++ seggit/data/scripts/make_semseg_target.py
@@ -7,11 +7,12 @@ import pandas as pd
 import cv2
 
 from seggit.data.config import DIR_BASE
-from seggit.data.util import annotation_to_semg
+from seggit.data.util import annotation_to_semg, get_semg_multicell
 
 
 train = pd.read_csv(f'{DIR_BASE}/train.csv')
 
+print('Calculating cell areas...')
 cell_area_list = []
 for cell in tqdm(train.itertuples(), total=len(train)):
     semg = annotation_to_semg(cell.annotation)
@@ -23,37 +24,36 @@ imgids = train['id'].unique()
 dir_out = '/kaggle/working/semg'
 os.makedirs(dir_out, exist_ok=True)
 
+args_list = [(train, dir_out, imgid) for imgid in imgids]
+
 def _generate_semg(args):
     train, dir_out, imgid = args
 
     df = train[train['id'] == imgid]
-    h_img, w_img = df[['height', 'width']].iloc[0]
+    image_height, image_width = df[['height', 'width']].iloc[0]
 
-    df = df.sort_values('cell_area', axis=0, ascending=False)
+    mass, border = get_semg_multicell(df, image_height, image_width, 
+                                      square_width=3)
 
-    semg = np.zeros((h_img, w_img, 1), dtype=np.float32)
-    for cell in df.itertuples():
-        semg_cell = annotation_to_semg(cell.annotation,
-                                       image_height=h_img, image_width=w_img)
-        pxs_cell = semg_cell[:, :, 0].astype(np.bool)
-        semg[pxs_cell, :] = 1
-
+    ch2 = np.zeros_like(mass)
+    semg = np.concatenate([mass, border, ch2], axis=2)
     semg = 255 * semg
     semg = semg.astype(np.uint8)
-    semg = np.stack([semg[..., 0], semg[..., 0], semg[..., 0]], axis=2)
+    semg[..., 2] = 255 - semg[..., 0] - semg[..., 1]
 
-    cv2.imwrite(f'{dir_out}/{imgid}.png', 
-                semg, 
+    cv2.imwrite(f'{dir_out}/{imgid}.png', semg, 
                 [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
     return imgid
 
-args_list = [(train, dir_out, imgid) for imgid in imgids]
 
+print('Generate semantic segmentation targets...')
 p = multiprocessing.Pool(processes=os.cpu_count())
 with tqdm(total=len(args_list)) as pbar:
     for imgid in p.imap(_generate_semg, args_list):
         pbar.set_description(f'Processed {imgid}')
         pbar.update(1)
 p.close()
+
+print('finished.')
 
