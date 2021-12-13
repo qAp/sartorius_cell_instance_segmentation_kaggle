cff2f3b6324f1b66e4eeafc11b6c5dad46f23e20
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-13 23:00:05 +0800

    Reduce the probability of albumentations in general

--- seggit/data/transforms.py
+++ seggit/data/transforms.py
@@ -10,33 +10,30 @@ def default_tfms(image_size):
 def aug_tfms(image_size):
     return [
         albu.HorizontalFlip(p=0.5),
-        albu.ShiftScaleRotate(shift_limit=0.2,
+        albu.ShiftScaleRotate(shift_limit=0.,
                               scale_limit=0.3,
-                              rotate_limit=180,
-                              p=1.,
+                              rotate_limit=45,
+                              p=.7,
                               border_mode=0),
-        albu.PadIfNeeded(min_height=520,
-                         min_width=520,
-                         always_apply=True,
-                         border_mode=0),
-        albu.RandomCrop(height=image_size,
-                        width=image_size,
+        albu.PadIfNeeded(min_height=520, min_width=520,
+                         always_apply=True, border_mode=0),
+        albu.RandomCrop(height=image_size, width=image_size,
                         always_apply=True),
-        albu.GaussNoise(p=1),
-        albu.Perspective(p=1),
+        albu.GaussNoise(p=.5),
+        albu.Perspective(p=.2),
         albu.OneOf(
             [
-                albu.CLAHE(p=1),
-                albu.RandomBrightnessContrast(p=1),
-                albu.RandomGamma(p=1)
+                albu.CLAHE(p=.5),
+                albu.RandomBrightnessContrast(p=.5),
+                albu.RandomGamma(p=.2)
             ],
-            p=0.9),
+            p=0.4),
         albu.OneOf(
             [
-                albu.Sharpen(p=1),
-                albu.Blur(blur_limit=5, p=1),
-                albu.MotionBlur(blur_limit=5, p=1)
+                albu.Sharpen(p=.3),
+                albu.Blur(blur_limit=5, p=.3),
+                albu.MotionBlur(blur_limit=5, p=.3)
             ],
-            p=0.9),
-        albu.HueSaturationValue(p=0.9)
+            p=0.5),
+        albu.HueSaturationValue(p=0.4)
     ]
