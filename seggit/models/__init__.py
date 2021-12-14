c6f589a7c71b223d28b3ba2f56c15ab5ea3bb36b
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-14 23:19:03 +0800

    Import shortcut for DirectionNetMock

--- seggit/models/__init__.py
+++ seggit/models/__init__.py
@@ -1,3 +1,4 @@
 from segmentation_models_pytorch import Unet
 from .direction_net import DirectionNet
+from .direction_net import DirectionNetMock
 from .watershed_transform_net import WatershedTransformNet
