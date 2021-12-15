7e0656756d36c0bf58a437619009c845e34c012d
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-16 00:13:48 +0800

    Add import shortcut for InstanceDirectionMockLitModel

--- seggit/lit_models/__init__.py
+++ seggit/lit_models/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseLitModel
 from .instance_direction_litmodel import InstanceDirectionLitModel
+from .instance_direction_mock_litmodel import InstanceDirectionMockLitModel
 from .watershed_energy_litmodel import WatershedEnergyLitModel
 from .semseg_litmodel import SemSegLitModel
