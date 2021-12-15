fa5433ebc0c0aa1a6445b385f629b09653693253
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-15 23:15:02 +0800

    Import shortcut for InstanceDirectionMock

--- seggit/data/__init__.py
+++ seggit/data/__init__.py
@@ -1,4 +1,5 @@
 from .cellclass import CellClass
 from .instance_direction import InstanceDirection
+from .instance_direction_mock import InstanceDirectionMock
 from .watershed_energy import WatershedEnergy
 from .semseg import SemSeg
