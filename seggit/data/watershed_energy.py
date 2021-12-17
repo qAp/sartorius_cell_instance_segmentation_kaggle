f753318db21c54775dde357ee31c3f0701c334f0
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-17 14:52:13 +0800

    Remove unneeded function
    Data transforms have been moved to data.transform.

--- seggit/data/watershed_energy.py
+++ seggit/data/watershed_energy.py
@@ -18,40 +18,6 @@ BATCH_SIZE = 8
 NUM_WORKERS = 0
 
 
-def _tfms(image_size):
-    return [
-        albu.HorizontalFlip(p=0.5),
-        albu.ShiftScaleRotate(shift_limit=0.2,
-                              scale_limit=0.3,
-                              rotate_limit=180,
-                              p=1.,
-                              border_mode=0),
-        albu.PadIfNeeded(min_height=520,
-                         min_width=520,
-                         always_apply=True,
-                         border_mode=0),
-        albu.RandomCrop(height=image_size,
-                        width=image_size,
-                        always_apply=True),
-        albu.GaussNoise(p=1),
-        albu.Perspective(p=1),
-        albu.OneOf(
-            [
-                albu.CLAHE(p=1),
-                albu.RandomBrightnessContrast(p=1),
-                albu.RandomGamma(p=1)
-            ],
-            p=0.9),
-        albu.OneOf(
-            [
-                albu.Sharpen(p=1),
-                albu.Blur(blur_limit=5, p=1),
-                albu.MotionBlur(blur_limit=5, p=1)
-            ],
-            p=0.9),
-        albu.HueSaturationValue(p=0.9)
-    ]
-
 
 class WatershedEnergyDataset(torch.utils.data.Dataset):
     def __init__(self, df, transform=None):
