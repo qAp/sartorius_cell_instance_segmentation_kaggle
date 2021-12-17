71e16134297a19d20508753392eede95376cd993
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-17 15:56:37 +0800

    DBG. Compute cell area for train.csv first

--- seggit/data/scripts/generate_instance_area.py
+++ seggit/data/scripts/generate_instance_area.py
@@ -5,13 +5,21 @@ from tqdm.auto import tqdm
 import numpy as np
 import pandas as pd
 from seggit.data.config import DIR_BASE
-from seggit.data.util import generate_instance_area
+from seggit.data.util import annotation_to_semg, generate_instance_area
 
 
 dir_area = '/kaggle/working/instance_area'
 os.makedirs(dir_area, exist_ok=True)
 
 train = pd.read_csv(f'{DIR_BASE}/train.csv')
+
+print('Calculating cell areas...')
+cell_area_list = []
+for cell in tqdm(train.itertuples(), total=len(train)):
+    semg = annotation_to_semg(cell.annotation)
+    cell_area_list.append(semg.sum())
+train['cell_area'] = np.array(cell_area_list)
+
 imgids = train['id'].unique()
 
 args_list = [(train, imgid, dir_area) for imgid in imgids]
@@ -24,7 +32,7 @@ def _generate_instance_area(args):
 
     area = generate_instance_area(df, image_height, image_width)
     np.save(f'{dir_area}/{imgid}', area)
-    
+
     return imgid
 
 p = multiprocessing.Pool(processes=os.cpu_count())
