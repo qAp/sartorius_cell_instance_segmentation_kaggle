14cc7489d70e7e5feb7d4955ffbc10495074953b
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-20 23:14:47 +0800

    Set default optimization parameters for end-to-end
    According to the paper.

--- seggit/lit_models/watershed_litmodel.py
+++ seggit/lit_models/watershed_litmodel.py
@@ -9,9 +9,10 @@ from seggit.lit_models.metrics import NCorrectPredictions
 
 
 
-LR = 5e-4
+LR = 5e-6
+WEIGHT_DECAY = 1e-6
 OPTIMIZER = 'Adam'
-ONE_CYCLE_TOTAL_STEPS = 100
+ONE_CYCLE_TOTAL_STEPS = 20
 LOSS = 'WatershedEnergyLoss1'
 
 
@@ -60,7 +61,7 @@ class WatershedLitModel(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = self.optimizer(self.parameters(), 
                                    lr=self.lr, 
-                                   weight_decay=1e-5)
+                                   weight_decay=WEIGHT_DECAY)
         if self.one_cycle_max_lr is None:
             return optimizer
         else:
