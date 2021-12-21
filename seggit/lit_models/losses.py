8aecebb3217d0ccf8001c5601aa07d2c1a6c8452
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-21 10:13:54 +0800

    Softmax Dice loss
    For training Unet with softmax head

--- seggit/lit_models/losses.py
+++ seggit/lit_models/losses.py
@@ -134,3 +134,50 @@ class WatershedEnergyLoss1(nn.Module):
 
         return cs / (weight.sum() + 1)
 
+
+def dice_coef(y_pred, y_true, smooth=1e-3):
+    '''
+    Args:
+        y_pred (N, H, W)    
+        y_true (N, H, W)
+    '''
+    y_true = y_true.reshape(-1)
+    y_pred = y_pred.reshape(-1)
+    
+    intersection = (y_true * y_pred).sum()
+    coef = (2*intersection + smooth) / (y_true.sum() + y_pred.sum() + smooth)
+    
+    return coef
+
+
+def dice_coef_loss(y_pred, y_true):
+    '''
+    Args:
+        y_pred (N, H, W)
+        y_true (N, H, W)        
+    '''    
+    return 1 - dice_coef(y_pred, y_true)
+
+
+class SoftmaxDiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cross_entropy = nn.CrossEntropyLoss()
+        
+    def forward(self, logits, semseg):
+        '''
+        Args:
+            logits (N, C, H, W): Softmax probability
+            semseg (N, C, H, W): Semantic segmentation (one-hot).
+            
+        Notes:
+            C=0 Cell
+            C=1 Overlapping cell wall
+            C=2 Background
+        '''
+        loss = (
+            .6 * self.cross_entropy(logits, semseg.argmax(dim=1)) + 
+            .2 * dice_coef_loss(logits[:, 0, ...], semseg[:, 0, ...]) +
+            .2 * dice_coef_loss(logits[:, 1, ...], semseg[:, 1, ...])
+        )
+        return loss
