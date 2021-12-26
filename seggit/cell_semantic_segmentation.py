4b2e96b1ada624395f23b7ea186aa6bed2e130c6
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-26 16:29:49 +0800

    Create class for semantic segmentation inference
    SemanticSegmenter is used to load a trained model
    and predict the semantic segmentation given the
    image path.

---
+++ seggit/cell_semantic_segmentation.py
@@ -0,0 +1,88 @@
+
+import os, sys
+import argparse
+import numpy as np
+import torch
+import cv2
+
+from seggit.data.util import padto_divisible_by32
+from seggit.data.config import MEAN_IMAGE, STD_IMAGE
+from seggit.data import SemSeg
+from seggit.models.util import create_segmentation_model
+from seggit.lit_models import SemSegLitModel
+
+
+
+def load_semseg_litmodel(checkpoint_path=None):
+    parser = argparse.ArgumentParser()
+    SemSeg.add_argparse_args(parser)
+    SemSegLitModel.add_argparse_args(parser)
+    args = parser.parse_args([
+        '--use_softmax', 
+        '--encoder_name', 'resnet152',
+    ])
+    
+    data = SemSeg(args)
+    model = create_segmentation_model(data.config(), args)
+    
+    lit_model = SemSegLitModel.load_from_checkpoint(
+        checkpoint_path=checkpoint_path, 
+        model=model)
+    
+    lit_model.eval()
+    return lit_model
+
+
+class SemanticSegmenter:
+
+    def __init__(self, checkpoint_path=None):
+        self.device = torch.device('cuda' if torch.cuda.is_available()
+                                   else 'cpu')        
+
+        self.model = load_semseg_litmodel(checkpoint_path)
+        self.mode.to(self.device)
+
+    @torch.no_grad()
+    def predict_semseg(self, img):
+        '''
+        Args:
+            img (N, H, W, 3) np.array: Normalised image.
+        Returns:
+            semseg (N, H, W, 3) np.array float: Semantic segmentation. 
+                Values 0 or 1.
+                Channel 0 == cell.  
+                Channel 1 == overlap wall. 
+                Channel 2 == everything else.
+        '''
+        img = torch.from_numpy(img).permute(0, 3, 1, 2)
+
+        img = img.to(self.device)
+        logits = self.model(img)
+        
+        semseg = logits.argmax(dim=1, keepdim=True)
+        semseg = torch.cat([semseg==0, semseg==1, semseg==2], dim=1)
+        semseg = semseg.type(torch.float32)    
+        semseg = semseg.permute(0, 2, 3, 1).data.cpu().numpy()
+        return semseg    
+
+    @torch.no_grad()
+    def predict(self, pth_img):
+        '''
+        Args:
+            pth_img [str, iter[str]]: Path(s) to image file(s).
+        Returns:
+            semseg (N, H, W, 3) np.array: Semantic segmentation
+        '''
+        # sample
+        img = cv2.imread(pth_img)
+        pad_img, img = padto_divisible_by32(img)
+        img = img.astype(np.float32)
+        img = (img - MEAN_IMAGE) / STD_IMAGE
+
+        # batch
+        img = img[None, ...]
+        semseg = self.predict_semseg(img)
+
+        return semseg
+
+
