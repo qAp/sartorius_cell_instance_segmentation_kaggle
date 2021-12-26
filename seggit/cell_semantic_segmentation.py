467928364f3ded66ee0474a70fd8e7e3ff9ee287
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-26 23:18:43 +0800

    TTA for semantic segmentation

--- seggit/cell_semantic_segmentation.py
+++ seggit/cell_semantic_segmentation.py
@@ -35,7 +35,9 @@ def load_semseg_litmodel(checkpoint_path=None):
 
 class SemanticSegmenter:
 
-    def __init__(self, checkpoint_path=None):
+    def __init__(self, checkpoint_path=None, tta=False):
+        self.checkpoint_path = checkpoint_path
+        self.tta = tta
         self.device = torch.device('cuda' if torch.cuda.is_available()
                                    else 'cpu')        
 
@@ -43,27 +45,49 @@ class SemanticSegmenter:
         self.model.to(self.device)
 
     @torch.no_grad()
-    def predict_semseg(self, img):
+    def predict_logits(self, img):
         '''
         Args:
             img (N, H, W, 3) np.array: Normalised image.
         Returns:
-            semseg (N, H, W, 3) np.array float: Semantic segmentation. 
-                Values 0 or 1.
+            logits (N, H, W, 3) np.array float: Model logits. 
                 Channel 0 == cell.  
                 Channel 1 == overlap wall. 
                 Channel 2 == everything else.
         '''
         img = torch.from_numpy(img).permute(0, 3, 1, 2)
-
         img = img.to(self.device)
         logits = self.model(img)
+        logits = logits.permute(0, 2, 3, 1).data.cpu().numpy()
+        return logits
+
+    def predict_logits_tta(self, img):
+        '''
+        Args:
+            img (N, H, W, 3) np.array: Normalised image.
+        Returns:
+            logits (N, H, W, 3) np.array float: Model logits.            
+        '''
+        do_flipud = [True, False]
+        rot_angles = [0, 90, 180, 270]
+
+        batch_size, height, width, _ = img.shape
         
-        semseg = logits.argmax(dim=1, keepdim=True)
-        semseg = torch.cat([semseg==0, semseg==1, semseg==2], dim=1)
-        semseg = semseg.type(torch.float32)    
-        semseg = semseg.permute(0, 2, 3, 1).data.cpu().numpy()
-        return semseg    
+        logits = np.zeros((batch_size, height, width, 3), np.float32)
+        for j, flipud in enumerate(do_flipud):
+            for i, angle in enumerate(rot_angles):
+                mm = img[:,::-1,:,:] if flipud else img
+                mm = np.rot90(mm, k=angle//90, axes=(1, 2))
+                
+                ll = self.predict_logits(mm)
+
+                ll = np.rot90(ll, k=(360 - angle)//90, axes=(1, 2))
+                ll = ll[:,::-1,:,:] if flipud else ll
+
+                logits += ll
+
+        logits /= (len(do_flipud) * len(rot_angles))
+        return logits
 
     @torch.no_grad()
     def predict(self, pth_img):
@@ -78,10 +102,16 @@ class SemanticSegmenter:
         pad_img, img = padto_divisible_by32(img)
         img = img.astype(np.float32)
         img = (img - MEAN_IMAGE) / STD_IMAGE
+        img = img[None, ...] 
+ 
+        if self.tta:
+            logits = self.predict_logits_tta(img)
+        else:
+            logits = self.predict_logits(img)
 
-        # batch
-        img = img[None, ...]
-        semseg = self.predict_semseg(img)
+        semseg = logits.argmax(axis=3)[..., None]
+        semseg = np.concatenate([semseg==0, semseg==1, semseg==2], axis=3)
+        semseg = semseg.astype(np.float32)
 
         semseg = semseg[:, 
                         pad_img['y0']: -pad_img['y1'],
