7c51219df5cf2aecaba778cad3ed051dd07cd275
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-30 01:33:51 +0800

    Import shortcut for InstanceDirectionBG

--- seggit/data/__init__.py
+++ seggit/data/__init__.py
@@ -1,6 +1,7 @@
 from .cellclass import CellClass
 from .instance_direction import InstanceDirection
 from .instance_direction_mock import InstanceDirectionMock
+from .instance_direction_bg import InstanceDirectionBG
 from .watershed_energy import WatershedEnergy
 from .watershed import Watershed
 from .semseg import SemSeg
