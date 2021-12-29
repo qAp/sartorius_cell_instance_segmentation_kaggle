a9b0ad54d0844f2c5bce18818330b27d7b96162b
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-30 02:40:12 +0800

    Make 32-separated multiclass semantic mask in forward()

--- seggit/models/direction_net_bg.py
+++ seggit/models/direction_net_bg.py
@@ -40,132 +40,8 @@ def net_params():
     return params
 
 
-def max_to_avgpool(m):
-    assert isinstance(m, nn.MaxPool2d)
-    return nn.AvgPool2d(kernel_size=m.kernel_size,
-                        stride=m.stride,
-                        padding=m.padding,
-                        ceil_mode=m.ceil_mode)
 
-
-class FCN(nn.Module):
-    def __init__(self, channels=32):
-        super().__init__()
-
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=channels,
-                      kernel_size=5, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=channels,
-                      kernel_size=1, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=channels,
-                      kernel_size=1, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        return x
-
-
-class Fuse(nn.Module):
-    def __init__(self, in_channels=1280, out_channels=2):
-        super().__init__()
-        
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
-                      kernel_size=1, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=in_channels,
-                      kernel_size=1, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-         
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                      kernel_size=1, stride=1, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        return x
-
-
-class DirectionNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        m = torchvision.models.vgg16(pretrained=True).features
-
-        self.conv1 = m[:4]
-        self.pool1 = m[4]
-        self.conv2 = m[5:9]
-        self.pool2 = m[9]
-        self.conv3 = m[10:16]
-        self.pool3 = m[16]
-        self.conv4 = m[17:23]
-        self.pool4 = m[23]
-        self.conv5 = m[24:30]
-
-        self.pool3 = max_to_avgpool(self.pool3)
-        self.pool4 = max_to_avgpool(self.pool4)
-
-        self.fcn3 = FCN(channels=256)
-        self.fcn4 = FCN(channels=512)
-        nn.init.xavier_normal_(self.fcn4.conv3[0].weight, gain=3)
-        self.fcn5 = FCN(channels=512)
-        nn.init.xavier_normal_(self.fcn5.conv3[0].weight, gain=8)
-
-        self.fuse3 = Fuse(in_channels=1280, out_channels=2)
-
-    def forward(self, x):
-        input_size = x.shape[-2:]
-
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.conv3(x)
-
-        x3 = self.fcn3(x)
-
-        x = self.pool3(x)
-        x = self.conv4(x)
-
-        x4 = self.fcn4(x)
-        x4 = nn.Upsample(x3.shape[-2:])(x4)
-
-        x = self.pool4(x)
-        x = self.conv5(x)
-
-        x5 = self.fcn5(x)
-        x5 = nn.Upsample(x3.shape[-2:])(x5)
-
-        x = torch.cat([x3, x4, x5], dim=1)
-
-        x = self.fuse3(x)
-
-        x = nn.Upsample(input_size)(x)
-
-        return x
-
-
-class DirectionNetMock(nn.Module):
+class DirectionNetBG(nn.Module):
     def __init__(self, data_config=None, args=None):
         super().__init__()
 
@@ -248,13 +124,28 @@ class DirectionNetMock(nn.Module):
                 'conv4_1': 17, 'conv4_2': 19, 'conv4_3': 21,
                 'conv5_1': 24, 'conv5_2': 26, 'conv5_3': 28}   
 
-    def forward(self, img, semg):
+    def forward(self, img, semseg):
         '''
         Args:
             img (N, 3x1, H, W )
-            semg (N, 1, H, W)
+            semseg (N, 3, H, W)
+
         '''
-        x = torch.cat([semg * img, 32 * semg], dim=1)
+        ss = semseg.sum(dim=1, keepdim=True)    # ss (N, 1, H, W)
+
+        nothing = torch.zeros_like(semseg[:,[0],:,:])
+        ssmask = torch.cat(
+            [
+                semseg[:,[1, 2],:,:].sum(dim=1, keepdim=True), 
+                nothing, 
+                semseg[:,[0],:,:]
+            ], 
+            dim=1)
+        ssmask = ssmask.argmax(dim=1, keepdim=True)
+        ssmask = ssmask.type(torch.float32)
+        ssmask = 32 * (ssmask - 1)             # ssmssk (N, 1, H, W)
+
+        x = torch.cat([ss * img, ssmask], dim=1)
 
         x = self.conv1_1(x)
         x = self.conv1_2(x)
@@ -301,7 +192,7 @@ class DirectionNetMock(nn.Module):
 
         x = self.upscore_layer(x)
 
-        x = semg * x
+        x = ss * x
 
         x = F.normalize(x, dim=1)
         return x
