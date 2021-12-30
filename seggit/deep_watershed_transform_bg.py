154c024970906b01530390c2f81dcfd4a1d647de
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-30 22:14:34 +0800

    Background-inclusive version of WN inference

--- seggit/deep_watershed_transform_bg.py
+++ seggit/deep_watershed_transform_bg.py
@@ -4,14 +4,14 @@ from seggit.data.util import padto_divisible_by32
 import torch
 
 from seggit.data.config import WATERSHED_ENERGY_BINS
-from seggit.models import WatershedNet
-from seggit.lit_models import WatershedLitModel
+from seggit.models import WatershedNetBG
+from seggit.lit_models import WatershedBGLitModel
 
 
 def load_watershed_litmodel(checkpoint_path=None):
-    model = WatershedNet()
+    model = WatershedNetBG()
     
-    lit_model = WatershedLitModel.load_from_checkpoint(
+    lit_model = WatershedBGLitModel.load_from_checkpoint(
         checkpoint_path=checkpoint_path,
         model=model)
     
@@ -19,7 +19,7 @@ def load_watershed_litmodel(checkpoint_path=None):
     return lit_model
 
 
-class DeepWatershedTransform:
+class DeepWatershedBGTransform:
     def __init__(self, checkpoint_path=None):
         self.checkpoint_path = checkpoint_path
         self.device = torch.device('cuda' if torch.cuda.is_available() 
@@ -29,34 +29,34 @@ class DeepWatershedTransform:
         self.model.to(self.device)
 
     @torch.no_grad()
-    def predict_logits(self, img, semg):
+    def predict_logits(self, img, semseg):
         '''
         Args:
             img (np.array[float]): 
                 Normalised image. Shape (N, H, W, 3) 
-            semg  (np.array[float]): 
-                Semantic segmentation. Shape (N, H, W, 1)
+            semseg  (np.array[float]): 
+                Semantic segmentation. Shape (N, H, W, 3)
 
         Returns:
             logits (np.array[float]): Logits. Shape (N, H, W, 17)
         '''
         img = torch.from_numpy(img).permute(0, 3, 1, 2)
-        semg = torch.from_numpy(semg).permute(0, 3, 1, 2)
+        semseg = torch.from_numpy(semseg).permute(0, 3, 1, 2)
         
         img = img.to(self.device)
-        semg = semg.to(self.device)
-        logits = self.model(img, semg)
+        semseg = semseg.to(self.device)
+        logits = self.model(img, semseg)
 
         logits = logits.permute(0, 2, 3, 1).data.cpu().numpy()
         return logits
 
-    def predict_logits_tta(self, img, semg):
+    def predict_logits_tta(self, img, semseg):
         '''
         Args:
             img (np.array[float]): 
                 Normalised image. Shape (N, H, W, 3) 
-            semg  (np.array[float]): 
-                Semantic segmentation. Shape (N, H, W, 1)
+            semseg  (np.array[float]): 
+                Semantic segmentation. Shape (N, H, W, 3)
 
         Returns:
             logits (np.array[float]): Logits. Shape (N, H, W, 17)
@@ -74,7 +74,7 @@ class DeepWatershedTransform:
         for flipud in do_flipud:
             for angle in rot_angles:
                 mm = img[:,::-1,:,:] if flipud else img
-                ss = semg[:,::-1,:,:] if flipud else semg
+                ss = semseg[:,::-1,:,:] if flipud else semseg
                 mm = np.rot90(mm, k=angle//90, axes=(1, 2))
                 ss = np.rot90(ss, k=angle//90, axes=(1, 2))
 
@@ -88,13 +88,13 @@ class DeepWatershedTransform:
         logits /= (len(do_flipud) * len(rot_angles))
         return logits
 
-    def predict(self, img, semg, tta=False):
+    def predict(self, img, semseg, tta=False):
         '''
         Args:
             img (np.array[float]): 
                 Normalised image. Shape (H, W, 3) 
-            semg  (np.array[float]): 
-                Semantic segmentation. Shape (H, W, 1)
+            semseg  (np.array[float]): 
+                Semantic segmentation. Shape (H, W, 3)
 
         Returns:
             wngy (np.array[float]): 
@@ -103,14 +103,13 @@ class DeepWatershedTransform:
         pad_img, img = padto_divisible_by32(img, mode='symmetric')
         img = img[None, ...]
 
-        pad_semg, semg = padto_divisible_by32(semg, mode='symmetric')
-        semg = semg[None, ...]
-
+        pad_semg, semseg = padto_divisible_by32(semseg, mode='symmetric')
+        semseg = semseg[None, ...]
 
         if tta:
-            logits = self.predict_logits_tta(img, semg)
+            logits = self.predict_logits_tta(img, semseg)
         else:
-            logits = self.predict_logits(img, semg)
+            logits = self.predict_logits(img, semseg)
 
         wngy = logits.argmax(axis=3)[..., None]
         wngy = wngy.astype(np.float32)
