4e6df076e26796d833ef2008be231dc11da0a31f
Jack Yu <jackchungchiehyu@gmail.com>
2021-12-30 15:53:59 +0800

    Import shortcut

--- seggit/data/__init__.py
+++ seggit/data/__init__.py
@@ -3,6 +3,7 @@ from .instance_direction import InstanceDirection
 from .instance_direction_mock import InstanceDirectionMock
 from .instance_direction_bg import InstanceDirectionBG
 from .watershed_energy import WatershedEnergy
+from .watershed_energy_bg import WatershedEnergyBG
 from .watershed import Watershed
 from .semseg import SemSeg
 
